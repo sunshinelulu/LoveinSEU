89607d73df0e37b165a7428f12b242e1321e289d
sunshinelulu <1367695045@qq.com>
2017-01-06 14:58:18 +0800

    make the project suitable in my own machine

--- app/dbSetting/__init__.py
+++ app/dbSetting/__init__.py
@@ -4,7 +4,10 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 #sqlurl = "mysql://root:root@localhost:3306/flasktestdb?charset=utf8"
+#sqlurl = "mysql://root:SEUqianshou2015@218.244.147.240:3306/flasktestdb?charset=utf8"
+#实验室数据库地址
 sqlurl = "mysql://root:SEUqianshou2015@218.244.147.240:3306/flasktestdb?charset=utf8"
 #sqlurl = "mysql://root:0596@223.3.36.246:3306/flasktestdb?charset=utf8"
 def create_app():
